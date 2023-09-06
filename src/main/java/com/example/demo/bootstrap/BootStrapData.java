954e8a62db4413cb3ea401d6b350f64268b7f086
Tcarri <133730561+Tcarri@users.noreply.github.com>
2023-09-06 15:56:47 -0700

    E.  Add a sample inventory appropriate for your chosen store to the application. You should have five parts and five products in your sample inventory and should not overwrite existing data in the database.

--- src/main/java/com/example/demo/bootstrap/BootStrapData.java
+++ src/main/java/com/example/demo/bootstrap/BootStrapData.java
@@ -39,33 +39,67 @@ public class BootStrapData implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-       /*
+
         OutsourcedPart o= new OutsourcedPart();
-        o.setCompanyName("Western Governors University");
-        o.setName("out test");
+        o.setCompanyName("VVNAZ Martial Arts");
+        o.setName("Belts");
         o.setInv(5);
         o.setPrice(20.0);
         o.setId(100L);
+        OutsourcedPart o2= new OutsourcedPart();
+        o2.setName("Tops");
+        o2.setInv(5);
+        o2.setPrice(20.0);
+        o2.setId(101L);
+        OutsourcedPart o3= new OutsourcedPart();
+        o3.setName("Pants");
+        o3.setInv(5);
+        o3.setPrice(20.0);
+        o3.setId(102L);
+        OutsourcedPart o4= new OutsourcedPart();
+        o4.setName("Helmets");
+        o4.setInv(5);
+        o4.setPrice(20.0);
+        o4.setId(103L);
+        OutsourcedPart o5= new OutsourcedPart();
+        o5.setName("Chest Gear");
+        o5.setInv(5);
+        o5.setPrice(20.0);
+        o5.setId(104L);
+
         outsourcedPartRepository.save(o);
-        OutsourcedPart thePart=null;
+        outsourcedPartRepository.save(o2);
+        outsourcedPartRepository.save(o3);
+        outsourcedPartRepository.save(o4);
+        outsourcedPartRepository.save(o5);
+
+        OutsourcedPart thePart = null;
         List<OutsourcedPart> outsourcedParts=(List<OutsourcedPart>) outsourcedPartRepository.findAll();
-        for(OutsourcedPart part:outsourcedParts){
-            if(part.getName().equals("out test"))thePart=part;
+        for (int i = 0; i < outsourcedParts.size(); i++) {
+            OutsourcedPart part = outsourcedParts.get(i);
+            if (part.getName().equals("Belts")) thePart = part;
         }
 
         System.out.println(thePart.getCompanyName());
-        */
-        List<OutsourcedPart> outsourcedParts=(List<OutsourcedPart>) outsourcedPartRepository.findAll();
+
+        //List<OutsourcedPart> outsourcedParts=(List<OutsourcedPart>) outsourcedPartRepository.findAll();
         for(OutsourcedPart part:outsourcedParts){
             System.out.println(part.getName()+" "+part.getCompanyName());
         }
 
-        /*
-        Product bicycle= new Product("bicycle",100.0,15);
-        Product unicycle= new Product("unicycle",100.0,15);
-        productRepository.save(bicycle);
-        productRepository.save(unicycle);
-        */
+
+//below works fine dont forget to uncomment
+        Product uniforms = new Product("Uniform", 50.0, 20);
+        Product fightingGear = new Product("Fighting Gear", 50.0, 20);
+        Product weapons = new Product("weapons", 20.0, 20);
+        Product awards = new Product("Awards", 5.0, 100);
+        Product firstAide = new Product("First Aide", 20.0, 50);
+        productRepository.save(uniforms);
+        productRepository.save(fightingGear);
+        productRepository.save(weapons);
+        productRepository.save(awards);
+        productRepository.save(firstAide);
+
 
         System.out.println("Started in Bootstrap");
         System.out.println("Number of Products"+productRepository.count());
